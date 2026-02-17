70495a80457fb2bdbbcc1a00b9cc2cb8a594ad8b
Michio Avryant Gervaise <chiomichio.avryant95@gmail.com>
2026-02-18 00:26:05 +0700

    fix: use dynamic Port for deployment on Railway

--- PiCO.py
+++ PiCO.py
@@ -202,4 +202,5 @@ def health_check():
     return jsonify({'status': 'OK', 'message': 'API PiCo sedang berjalan normal'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000)) # ambil port dari environment variable atau default ke 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
