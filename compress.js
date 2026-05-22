166e1c7c8932d554b1a49f23c123cc8ededcd8e7
Michio Avryant Gervaise <chiomichio.avryant95@gmail.com>
2026-05-22 22:09:09 +0700

    feat: initial commit: update API_URL with the newest Pico backend API

--- compress.js
+++ compress.js
@@ -6,7 +6,7 @@ const cancelBtn = document.querySelector(".cancel-btn");
 const compressBtn = document.querySelector(".compress-btn-main");
 
 /// ==== KONFIGURASI ====
-const API_URL = "https://pico-production.up.railway.app/api/compress";
+const API_URL = "faster-pico.up.railway.app/api/compress"; // Ganti dengan URL backend yang tersedia
 
 // ===== LOAD IMAGE FROM INDEXEDDB =====
 let originalImage = null;
